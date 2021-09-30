491aa1adf180ba1f30e5538b14de77cc8f067bae
Marco <markosayajin@hotmail.com>
2021-09-30 17:04:28 -0500

    Merge

--- Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/UserService.java
+++ Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/UserService.java
@@ -1,9 +1,15 @@
 package com.example.banco_tiempo;
 
 
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 
 public interface UserService {
@@ -12,4 +18,10 @@ public interface UserService {
 
     @POST("register")
     Call<RegisterResponse> registerUser(@Body RegisterRequest registerRequest);
+
+    @Multipart
+    // POST request to upload an image from storage
+    @POST("uploadimage/")
+    Call<ResponseBody> uploadImage(@Part MultipartBody.Part image);
+
 }
