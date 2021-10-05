64eb020a7861baba80f178248f7d0126fb9c2f38
Angel009 <angelus007.ans@gmail.com>
2021-10-05 09:33:53 -0500

    Select, view and upload user img files
    Includes prev-view, base64, and URI location

--- Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/UserDocumentsActivity.java
+++ Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/UserDocumentsActivity.java
@@ -19,6 +19,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -40,16 +41,20 @@ public class UserDocumentsActivity extends AppCompatActivity {
     Uri selectedImage;
     String part_image;
     */
+    Uri selectedImage;
+    String part_image;
+
     TextView inePath, domPath, antPath, certPath;
     ImageView ine, dom, ant, cert;
     Button bIne, bDom, bAnt, bCert;
+    String sIne, sDom, sAnt, sCert;
 
     ActivityResultLauncher<String>ineGetContent;
     ActivityResultLauncher<String>domGetContent;
     ActivityResultLauncher<String>antGetContent;
     ActivityResultLauncher<String>certGetContent;
 
-    String sImage;
+
     // Permissions for accessing the storage
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
@@ -63,28 +68,61 @@ public class UserDocumentsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_documents);
 
-        inePath = findViewById(R.id.ine);
+        inePath = findViewById(R.id.rutaIne);
         ine = findViewById(R.id.ineView);
         bIne = findViewById(R.id.ine);
 
-        domPath = findViewById(R.id.dom);
+        domPath = findViewById(R.id.rutaDom);
         dom = findViewById(R.id.domView);
         bDom = findViewById(R.id.dom);
 
-        antPath = findViewById(R.id.ant);
+        antPath = findViewById(R.id.rutaAnt);
         ant = findViewById(R.id.antView);
         bAnt = findViewById(R.id.ant);
 
-        certPath = findViewById(R.id.cert);
+        certPath = findViewById(R.id.rutaCert);
         cert = findViewById(R.id.certView);
         bCert = findViewById(R.id.cert);
 
         ineGetContent=registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @SuppressLint("Range")
+            @RequiresApi(api = Build.VERSION_CODES.R)
             @Override
             public void onActivityResult(Uri result) {
                 ine.setImageURI(result);
+                selectedImage = MediaStore.Images.Media.getContentUri("external");                                                         // Get the image file URI
+                String[] imageProjection = {MediaStore.Images.Media.DATA,MediaStore.Images.Media.DISPLAY_NAME};
+                Cursor cursor = getContentResolver().query(selectedImage, imageProjection, null, null, null);
 
+                if (cursor.getCount()>0) {
+                    cursor.moveToPosition(0);
+                    part_image = cursor.getString(cursor.getColumnIndex(imageProjection[0]));
 
+                    //Uri filename = Uri.parse(imageProjection[0]);
+
+                    cursor.close();
+                   inePath.setText(part_image);
+                   bIne.setText("Documento cargado");
+
+                    // Get the image file absolute path
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100,stream);
+                        byte[] bytes = stream.toByteArray();
+                        sIne = Base64.encodeToString(bytes,Base64.DEFAULT);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    //image.setImageBitmap(bitmap);
+                    // Set the ImageView with the bitmap of the image
+                }
+
+                else {
+                    Toast.makeText(UserDocumentsActivity.this,"Algo Salio mal", Toast.LENGTH_SHORT);
+                }
             }
         });
 
@@ -97,10 +135,45 @@ public class UserDocumentsActivity extends AppCompatActivity {
         });
 
         domGetContent=registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @SuppressLint("Range")
+            @RequiresApi(api = Build.VERSION_CODES.R)
             @Override
             public void onActivityResult(Uri result) {
                 dom.setImageURI(result);
-                
+
+                selectedImage = MediaStore.Images.Media.getContentUri("external");                                                         // Get the image file URI
+                String[] imageProjection = {MediaStore.Images.Media.DATA,MediaStore.Images.Media.DISPLAY_NAME};
+                Cursor cursor = getContentResolver().query(selectedImage, imageProjection, null, null, null);
+
+                if (cursor.getCount()>0) {
+                    cursor.moveToPosition(0);
+                    part_image = cursor.getString(cursor.getColumnIndex(imageProjection[0]));
+
+                    //Uri filename = Uri.parse(imageProjection[0]);
+
+                    cursor.close();
+                    domPath.setText(part_image);
+                    bDom.setText("Documento cargado");
+
+                    // Get the image file absolute path
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100,stream);
+                        byte[] bytes = stream.toByteArray();
+                        sIne = Base64.encodeToString(bytes,Base64.DEFAULT);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    //image.setImageBitmap(bitmap);
+                    // Set the ImageView with the bitmap of the image
+                }
+
+                else {
+                    Toast.makeText(UserDocumentsActivity.this,"Algo Salio mal", Toast.LENGTH_SHORT);
+                }
             }
         });
 
@@ -113,9 +186,45 @@ public class UserDocumentsActivity extends AppCompatActivity {
         });
 
         antGetContent=registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @SuppressLint("Range")
+            @RequiresApi(api = Build.VERSION_CODES.R)
             @Override
             public void onActivityResult(Uri result) {
                 ant.setImageURI(result);
+
+                selectedImage = MediaStore.Images.Media.getContentUri("external");                                                         // Get the image file URI
+                String[] imageProjection = {MediaStore.Images.Media.DATA,MediaStore.Images.Media.DISPLAY_NAME};
+                Cursor cursor = getContentResolver().query(selectedImage, imageProjection, null, null, null);
+
+                if (cursor.getCount()>0) {
+                    cursor.moveToPosition(0);
+                    part_image = cursor.getString(cursor.getColumnIndex(imageProjection[0]));
+
+                    //Uri filename = Uri.parse(imageProjection[0]);
+
+                    cursor.close();
+                    antPath.setText(part_image);
+                    bAnt.setText("Documento cargado");
+
+                    // Get the image file absolute path
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100,stream);
+                        byte[] bytes = stream.toByteArray();
+                        sAnt = Base64.encodeToString(bytes,Base64.DEFAULT);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    //image.setImageBitmap(bitmap);
+                    // Set the ImageView with the bitmap of the image
+                }
+
+                else {
+                    Toast.makeText(UserDocumentsActivity.this,"Algo Salio mal", Toast.LENGTH_SHORT);
+                }
             }
         });
 
@@ -127,9 +236,45 @@ public class UserDocumentsActivity extends AppCompatActivity {
         });
 
         certGetContent=registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @SuppressLint("Range")
+            @RequiresApi(api = Build.VERSION_CODES.R)
             @Override
             public void onActivityResult(Uri result) {
                 cert.setImageURI(result);
+
+                selectedImage = MediaStore.Images.Media.getContentUri("external");                                                         // Get the image file URI
+                String[] imageProjection = {MediaStore.Images.Media.DATA,MediaStore.Images.Media.DISPLAY_NAME};
+                Cursor cursor = getContentResolver().query(selectedImage, imageProjection, null, null, null);
+
+                if (cursor.getCount()>0) {
+                    cursor.moveToPosition(0);
+                    part_image = cursor.getString(cursor.getColumnIndex(imageProjection[0]));
+
+                    //Uri filename = Uri.parse(imageProjection[0]);
+
+                    cursor.close();
+                    certPath.setText(part_image);
+                    bCert.setText("Documento cargado");
+
+                    // Get the image file absolute path
+                    Bitmap bitmap = null;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100,stream);
+                        byte[] bytes = stream.toByteArray();
+                        sCert = Base64.encodeToString(bytes,Base64.DEFAULT);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    //image.setImageBitmap(bitmap);
+                    // Set the ImageView with the bitmap of the image
+                }
+
+                else {
+                    Toast.makeText(UserDocumentsActivity.this,"Algo Salio mal", Toast.LENGTH_SHORT);
+                }
             }
         });
 
@@ -141,7 +286,7 @@ public class UserDocumentsActivity extends AppCompatActivity {
         });
     }
 
-    private String getRealPathFromURI(Uri contentURI) {
+    /*private String getRealPathFromURI(Uri contentURI) {
         String result;
         Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
         if (cursor == null) { // Source is Dropbox or other similar local file path
@@ -154,62 +299,37 @@ public class UserDocumentsActivity extends AppCompatActivity {
         }
         return result;
     }
+    */
+     /*private void preUpload(){
 
-    // Method for starting the activity for selecting image from phone storage
-   /* public void pick(View view) {
-        verifyStoragePermissions(UserDocumentsActivity.this);
-        mGetContent.launch("image/*");
+     }
+      */
+
+    // Upload the images to the remote database
+    public void uploadIne(View view) {
+        ImageRequest imageRequest = new ImageRequest();
+        imageRequest.setImage(sIne);
+        imageRequest.setUsername("JoseLuis");
+        uploadImageServer(imageRequest);
     }
 
-    ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
-            new ActivityResultCallback<Uri>() {
-                @SuppressLint("Range")
-                @RequiresApi(api = Build.VERSION_CODES.R)
-                @Override
-                public void onActivityResult(Uri uri) {
-                    selectedImage = MediaStore.Images.Media.getContentUri("external");                                                         // Get the image file URI
-                    String[] imageProjection = {MediaStore.Images.Media.DATA,MediaStore.Images.Media.DISPLAY_NAME};
-                    Cursor cursor = getContentResolver().query(selectedImage, imageProjection, null, null, null);
-                    if (cursor.getCount()>0) {
-                        cursor.moveToPosition(0);
-                        part_image = cursor.getString(cursor.getColumnIndex(imageProjection[0]));
-
-                        Uri filename = Uri.parse(imageProjection[0]);
-
-                        cursor.close();
-                        imgPath.setText(part_image);
-
-                        // Get the image file absolute path
-                        Bitmap bitmap = null;
-                        try {
-                            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
-                            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                            bitmap.compress(Bitmap.CompressFormat.PNG, 100,stream);
-                            byte[] bytes = stream.toByteArray();
-                            sImage = Base64.encodeToString(bytes,Base64.DEFAULT);
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        image.setImageBitmap(bitmap);
-                        //imgPath.setImageBitmap(bitmap);
-                        // Set the ImageView with the bitmap of the image
-                    }
-
-                    else {
-                        Toast.makeText(UserDocumentsActivity.this,"Algo Salio mal", Toast.LENGTH_SHORT);
-                    }
-
-                }
-            });
-
-    // Method to get the absolute path of the selected image from its URI
-
-*/
-    // Upload the image to the remote database
-    public void uploadImage(View view) {
+    public void uploadDom(View view) {
         ImageRequest imageRequest = new ImageRequest();
-        imageRequest.setImage(sImage);
+        imageRequest.setImage(sDom);
+        imageRequest.setUsername("JoseLuis");
+        uploadImageServer(imageRequest);
+    }
+
+    public void uploadAnt(View view) {
+        ImageRequest imageRequest = new ImageRequest();
+        imageRequest.setImage(sAnt);
+        imageRequest.setUsername("JoseLuis");
+        uploadImageServer(imageRequest);
+    }
+
+    public void uploadCert(View view) {
+        ImageRequest imageRequest = new ImageRequest();
+        imageRequest.setImage(sCert);
         imageRequest.setUsername("JoseLuis");
         uploadImageServer(imageRequest);
     }
