710ed151d38a000b9f1858605571260d80662fd8
OpaqueBlock <sushitofox@gmail.com>
2021-10-12 14:09:12 -0500

    Fixed Minor Problems

--- Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/UserDocumentsActivity.java
+++ Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/UserDocumentsActivity.java
@@ -1,32 +1,18 @@
 package com.example.banco_tiempo;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -34,9 +20,16 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 import retrofit2.Call;
@@ -203,6 +196,7 @@ public class UserDocumentsActivity extends AppCompatActivity {
                         if (null != uri) {
 
                             ine.setImageURI(uri);
+                            changeBtn(bIne, inePath);
                             Bitmap bitmap = null;
                             try {
                                 bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), uri);
@@ -232,6 +226,7 @@ public class UserDocumentsActivity extends AppCompatActivity {
                         if (null != uri) {
 
                             dom.setImageURI(uri);
+                            changeBtn(bDom, domPath);
                             Bitmap bitmap = null;
                             try {
                                 bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), uri);
@@ -261,6 +256,7 @@ public class UserDocumentsActivity extends AppCompatActivity {
                         if (null != uri) {
 
                             ant.setImageURI(uri);
+                            changeBtn(bAnt, antPath);
                             Bitmap bitmap = null;
                             try {
                                 bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), uri);
