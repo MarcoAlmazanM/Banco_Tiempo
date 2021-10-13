d4f0fb51b38294dde6d7fde4a7ffab5c01b0a8e5
Marco <markosayajin@hotmail.com>
2021-10-13 17:46:58 -0500

    List Offers

--- Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/ListOffersActivity.java
+++ Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/ListOffersActivity.java
@@ -45,6 +45,7 @@ public class ListOffersActivity extends AppCompatActivity implements SearchView.
     String categoria;
     String colonia;
     String message;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +63,7 @@ public class ListOffersActivity extends AppCompatActivity implements SearchView.
         editor = preferences.edit();
         categoria = preferences.getString("categoria","NULL");
         colonia = preferences.getString("colonia", "NULL");
+        username = preferences.getString("username","NULL");
 
         uploadEffect(chartView);
 
@@ -86,6 +88,7 @@ public class ListOffersActivity extends AppCompatActivity implements SearchView.
         OffersRequest offersRequest = new OffersRequest();
         offersRequest.setCategoria(categoria);
         offersRequest.setColonia(colonia);
+        offersRequest.setUsername(username);
         getOffers(offersRequest);
     }
 
--- Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/OffersRequest.java
+++ Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/OffersRequest.java
@@ -1,8 +1,17 @@
 package com.example.banco_tiempo;
 
 public class OffersRequest {
+    private String username;
     private String categoria;
     private String colonia;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
     public String getCategoria() {
         return categoria;
