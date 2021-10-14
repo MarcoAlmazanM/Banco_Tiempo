f57abae8449fb134603d12a22d1a9e85d3809735
Marco <markosayajin@hotmail.com>
2021-10-14 17:34:04 -0500

    JSON

--- Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/UserAcceptedServicesResponse.java
+++ Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/UserAcceptedServicesResponse.java
@@ -1,10 +1,12 @@
 package com.example.banco_tiempo;
 
 public class UserAcceptedServicesResponse {
-    private String image;
-    //private String ;
+    private Integer idNot;
+    private String idReceptor;
+    private String idEmisor;
     private Integer idServicio;
+    private String image;
+    private String nombre;
     private String categoria;
     private String descripcion;
-    private String nombre;
 }
