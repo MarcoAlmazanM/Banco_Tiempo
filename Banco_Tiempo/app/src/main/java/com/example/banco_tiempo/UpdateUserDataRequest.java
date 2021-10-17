6cdeba62e758cfc5f6a85a279c78af38a0ec2f98
Marco <markosayajin@hotmail.com>
2021-10-17 17:53:30 -0500

    Update User Data Response & Request

---
+++ Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/UpdateUserDataRequest.java
@@ -0,0 +1,85 @@
+package com.example.banco_tiempo;
+
+public class UpdateUserDataRequest {
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private String calle;
+    private Integer numero;
+    private Integer cp;
+    private String colonia;
+    private String municipio;
+    private String estado;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Integer getCp() {
+        return cp;
+    }
+
+    public void setCp(Integer cp) {
+        this.cp = cp;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
