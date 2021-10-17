c2d8e92fefe474c8f2bd2d6503b1624193c95ee4
OpaqueBlock <sushitofox@gmail.com>
2021-10-17 14:54:38 -0500

    Parameters Update

--- Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/UpdateUserData.java
+++ Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/UpdateUserData.java
@@ -1,20 +1,28 @@
 package com.example.banco_tiempo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class UpdateUserData extends AppCompatActivity {
 
     String nombre, apellidoM, apellidoP, calle, colonia, municipio, estado, numInterno, codPostal;
     EditText eTnombre, eTapellidoM, eTapellidoP, eTcalle, eTcolonia, eTmunicipio, eTestado, eTnumInterno, eTcodPostal;
 
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_user_data);
+
+        preferences = getSharedPreferences("userData", Context.MODE_PRIVATE);
+        editor = preferences.edit();
 
         eTnombre = findViewById(R.id.eTNombreU);
         eTapellidoM = findViewById(R.id.eTApellidoMU);
@@ -26,16 +34,24 @@ public class UpdateUserData extends AppCompatActivity {
         eTnumInterno = findViewById(R.id.eTNumeroIU);
         eTcodPostal = findViewById(R.id.eTCodigoPU);
 
-        eTnombre.setText("Texto de prueba", TextView.BufferType.EDITABLE);
+        setUserData();
+
+        eTnombre.setText(nombre, TextView.BufferType.EDITABLE);
         eTapellidoM.setText("Texto de prueba", TextView.BufferType.EDITABLE);
-        eTapellidoP.setText("Texto de prueba", TextView.BufferType.EDITABLE);
+        eTapellidoP.setText(apellidoP, TextView.BufferType.EDITABLE);
         eTcalle.setText("Texto de prueba", TextView.BufferType.EDITABLE);
-        eTcolonia.setText("Texto de prueba", TextView.BufferType.EDITABLE);
+        eTcolonia.setText(colonia, TextView.BufferType.EDITABLE);
         eTmunicipio.setText("Texto de prueba", TextView.BufferType.EDITABLE);
         eTestado.setText("Texto de prueba", TextView.BufferType.EDITABLE);
         eTnumInterno.setText("Texto de prueba", TextView.BufferType.EDITABLE);
         eTcodPostal.setText("Texto de prueba", TextView.BufferType.EDITABLE);
 
+    }
+
+    public void setUserData() {
+        nombre = preferences.getString("name","nombre");
+        apellidoP = preferences.getString("lastname", "apellidoP");
+        colonia = preferences.getString("colonia", "colonia");
     }
 
     public void getuserInfo(){
