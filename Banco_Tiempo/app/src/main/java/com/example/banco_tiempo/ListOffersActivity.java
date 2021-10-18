9754861b00bdf48dc71776c1a1a50698a65cbd2c
OpaqueBlock <sushitofox@gmail.com>
2021-10-18 14:13:45 -0500

    Adding Back Buttons

--- Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/ListOffersActivity.java
+++ Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/ListOffersActivity.java
@@ -3,20 +3,15 @@ package com.example.banco_tiempo;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -60,6 +55,9 @@ public class ListOffersActivity extends AppCompatActivity implements SearchView.
         toolbar = findViewById(R.id.toolbar);
         setTitle("Listado de Ofertas");
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         chartView = findViewById(R.id.pVprogressPie);
 
@@ -197,6 +195,12 @@ public class ListOffersActivity extends AppCompatActivity implements SearchView.
         //adapter=new ListAdapter(ofertas,this, adapter.action);
         adapter.filtro(s);
         return false;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 
 }
