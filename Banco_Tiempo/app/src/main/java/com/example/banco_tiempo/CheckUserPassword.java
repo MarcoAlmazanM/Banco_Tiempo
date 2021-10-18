a71db9a296d07857c2045d7f31ea8747a319c430
AdrianTorresG <atg292001@hotmail.com>
2021-10-18 17:45:52 -0500

    Password funcional

--- Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/CheckUserPassword.java
+++ Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/CheckUserPassword.java
@@ -3,11 +3,24 @@ package com.example.banco_tiempo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CheckUserPassword extends AppCompatActivity {
+
+    TextInputLayout tPassword;
+    EditText eTPassword;
+    String password, message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +29,61 @@ public class CheckUserPassword extends AppCompatActivity {
     }
 
     public void btnChangeUserPassword(View view){
-        Intent changePassword = new Intent(CheckUserPassword.this, ChangePassword.class);
-        startActivity(changePassword);
+        verifyData(view);
+    }
+
+    public void colorText(TextInputLayout myInputLayout, String myString) {
+
+        myInputLayout.getEditText().setTextColor(Color.parseColor("#ff0000"));
+        myInputLayout.getEditText().setText(myString);
+        myInputLayout.getEditText().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myInputLayout.getEditText().setTextColor(Color.BLACK);
+            }
+        });
+
+    }
+
+    public boolean validateContra() {
+        String regex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,255}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+
+        boolean flag = matcher.matches();
+
+        if (!flag) {
+            tPassword = findViewById(R.id.textInputUserContra);
+            colorText(tPassword, password);
+        }
+        return flag;
+    }
+
+    public void verifyData(View view){
+        // Find Id EditText
+        eTPassword = findViewById(R.id.eTUserPassword);
+
+        // Set strings UI
+        password = eTPassword.getText().toString();
+
+        if(TextUtils.isEmpty(password)){
+            message = "La contraseña es requerida.";
+            Toast.makeText(CheckUserPassword.this, message, Toast.LENGTH_LONG).show();
+        } else{
+            if (validateContra()) {
+
+                Intent changePassword = new Intent(CheckUserPassword.this, ChangePassword.class);
+                startActivity(changePassword);
+
+            }
+            else {
+                message = "La contraseña introducida no es correcta.";
+                Toast.makeText(CheckUserPassword.this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void userContra(){
+        tPassword = findViewById(R.id.textInputUserContra);
     }
 }
