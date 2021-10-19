49ed0ce8845968147271f5b543ab83d177788a0c
Marco <markosayajin@hotmail.com>
2021-10-19 02:29:47 -0500

    Gone Name REQUEST

--- Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/NotificationAdapter.java
+++ Banco_Tiempo/app/src/main/java/com/example/banco_tiempo/NotificationAdapter.java
@@ -261,12 +261,12 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
                 case 1:
                     a.setVisibility(View.VISIBLE);
                     r.setVisibility(View.VISIBLE);
-                    n.setVisibility(View.VISIBLE);
+                    n.setVisibility(View.GONE);
                     correo.setVisibility(View.INVISIBLE);
                     ap.setVisibility(View.VISIBLE);
                     am.setVisibility(View.VISIBLE);
                     correoPlaceholder.setVisibility(View.INVISIBLE);
-                    nombrePLaceholder.setVisibility(View.VISIBLE);
+                    nombrePLaceholder.setVisibility(View.GONE);
                     mensajeUsuario.setVisibility(View.GONE);
                     eliminar.setVisibility(View.GONE);
 
